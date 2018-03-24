ede897b58157d728022933bbdb332ab8b75a6567
Matt Hampel <mahatm@gmail.com>
2018-03-24 11:58:17 -0400

    Try setting a custom secret_key_base in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.secret_key_base = 'foo'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
