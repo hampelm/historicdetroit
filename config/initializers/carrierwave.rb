df37a99dc0cdd86e1aec37e19116847d7d64920b
Matt Hampel <mahatm@gmail.com>
2018-10-21 12:41:22 -0400

    Fix up secrets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
 CarrierWave.configure do |config|
+  secrets = Rails.application.credentials[Rails.env.to_sym]
   config.fog_provider = 'fog/google'
   config.fog_credentials = {
     provider: 'Google',
-    google_project: Rails.application.secrets.google_cloud_storage_project_name,
-    google_json_key_string: Rails.application.secrets.google_cloud_storage_credential_content
+    google_project: secrets[:google_cloud_storage_project_name],
+    google_json_key_string: secrets[:google_cloud_storage_credential_content]
   }
-  config.fog_directory = Rails.application.secrets.google_cloud_storage_bucket_name
+  config.fog_directory = secrets[:google_cloud_storage_bucket_name]
 end
 
 if Rails.env.development?
