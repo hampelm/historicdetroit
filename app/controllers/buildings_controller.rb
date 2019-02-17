0e8314be9fd4b1293006a265abe281fc29717879
Matt Hampel <mahatm@gmail.com>
2019-02-17 10:57:12 -0500

    Add fresh_when tags

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,9 +1,11 @@
 class BuildingsController < ApplicationController
   def index
     @buildings = Building.without_homes
+    fresh_when(@buildings)
   end
 
   def show
     @building = Building.friendly.find(params[:id])
+    fresh_when(@building)
   end
 end
