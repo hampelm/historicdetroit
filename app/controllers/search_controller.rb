47ec6b54e994bc1f8e2f896eb58a4f15296c0dc0
Matt Hampel <mahatm@gmail.com>
2019-03-31 10:59:54 -0400

    Try a simpler search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
-    @buildings = Building.search_for(@query)
+    @buildings = Building.where("description like ?", "%#{@query}%")
+    # Building.search_for(@query)
   end
 end
