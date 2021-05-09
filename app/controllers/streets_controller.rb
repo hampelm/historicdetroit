c5e78435c75250915d85524a8cb3a4d6f0e9dd4c
Matt Hampel <mahatm@gmail.com>
2021-05-09 12:00:24 -0400

    Sort streets alphabetically

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -1,5 +1,7 @@
 class StreetsController < ApplicationController
   def index
     @streets = Street.all
+    @streets_by_first_letter = @streets.group_by { |s| s.name[0] }
+    @streets_by_first_letter = @streets_by_first_letter.sort_by { |key| key }.to_h
   end
 end
