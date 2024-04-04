b8ef7431ba38abc151adfab4267ceb0053664137
Matt Hampel <mahatm@gmail.com>
2024-04-03 20:56:16 -0400

    Search by address too

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
-    @buildings = Building.where("description ILIKE ?", "%#{@query}%").or(Building.where("name ILIKE ?", "%#{@query}%"))
+    @buildings = Building.where("description ILIKE ?", "%#{@query}%").or(Building.where("name ILIKE ?", "%#{@query}%")).or(Building.where("address ILIKE ?", "%#{@query}%"))
 
 
   end
