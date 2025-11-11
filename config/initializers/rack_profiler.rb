7bc0e04defe7f2c9382c85d45683c30c6eec6a4a
Matt Hampel <mahatm@gmail.com>
2025-11-11 13:17:45 -0500

    Revert "Merge pull request #82 from hampelm/profile"
    This reverts commit 4e97db32d496eb96d1bea4f2979b38de9d4ca62c, reversing
    changes made to 59403ba4c52ff96090ed2453e9be1b6eeb421882.

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.production?
+if Rails.env.development?
   require "rack-mini-profiler"
 
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
-
-  if Rails.env.production?
-    # Only authorize admin users in production
-    Rack::MiniProfiler.config.pre_authorize_cb = lambda { |env|
-      warden = env['warden']
-      
-      # Check if user is logged in and is an admin
-      warden&.user&.admin == true
-    }
-  end
 end
