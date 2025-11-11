c311006ee1c9d937ebabcf4cc32f638f38836418
Matt Hampel <mahatm@gmail.com>
2025-11-11 10:46:20 -0500

    Remove some Heroku debugging stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :admin?
 
-  # Log full exception traces in production
-  rescue_from StandardError do |exception|
-    Rails.logger.error "=" * 80
-    Rails.logger.error "EXCEPTION IN CONTROLLER: #{exception.class}"
-    Rails.logger.error "MESSAGE: #{exception.message}"
-    Rails.logger.error "BACKTRACE:"
-    Rails.logger.error exception.backtrace.join("\n")
-    Rails.logger.error "=" * 80
-    
-    # Re-raise so Rails handles it normally (shows error page, etc)
-    raise exception
-  end
-
   before_action do
   end
 
