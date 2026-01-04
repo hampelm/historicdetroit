f6df751724b3fbfc46ef90e3042147d76bf13028
Matt Hampel <mahatm@gmail.com>
2026-01-04 10:22:17 -0500

    Homepage new

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.past.limit(25)
+
+    # Get buildings and galleries added recently
+    @new = Gallery.order('created_at DESC').limit(4).to_a
+    Building.order('created_at DESC').limit(4).each do |building|
+      @new << building
+    end
+    @new = @new.sort_by(&:created_at).reverse.take(4)
+    
   end
 end
