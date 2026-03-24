718ca78cd4c8683fde63a5692d37ee56906e47a4
Matt Hampel <mahatm@gmail.com>
2026-03-24 08:46:54 -0400

    Check if photo attached

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -3,7 +3,7 @@ module ImageHelper
   # http://www.imagemagick.org/script/command-line-processing.php#geometry
 
   def photo?
-    photo.attached?
+    photo && photo.attached?
   end
 
   def thumbnail
