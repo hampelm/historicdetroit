70ae7030de43d1827adc02b039517ea4970bbc38
Matt Hampel <mahatm@gmail.com>
2026-05-23 11:06:25 -0400

    AKA in search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,11 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
-    @buildings = Building.where("description ILIKE ?", "%#{@query}%").or(Building.where("name ILIKE ?", "%#{@query}%")).or(Building.where("address ILIKE ?", "%#{@query}%"))
-
+    @buildings = Building
+      .where("description ILIKE ?", "%#{@query}%")
+      .or(Building.where("name ILIKE ?", "%#{@query}%"))
+      .or(Building.where("address ILIKE ?", "%#{@query}%"))
+      .or(Building.where("also_known_as ILIKE ?", "%#{@query}%"))
 
   end
 end
